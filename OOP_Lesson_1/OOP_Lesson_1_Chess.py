0610944c1851a53cbf32997fb128f6944b2eb18a
caelmeasnadh <kishinskiyy@gmail.com>
2023-08-24 19:14:12 +0300

    updated

--- OOP_Lesson_1/OOP_Lesson_1_Chess.py
+++ OOP_Lesson_1/OOP_Lesson_1_Chess.py
@@ -2,12 +2,16 @@ class Chess:
     color_white = 'white'
     color_black = 'black'
 
+# В першому варіанті перевірка в мене повертала строку і не виконувалась в ініті, додаю її в ініт так, щоб повертала bool
     def __init__(self, color, place_on_the_board):
         self.color = color
-        self.place_on_the_board = place_on_the_board
+        if 0 <= place_on_the_board[0] <= 7 and 0 <= place_on_the_board[1] <= 7:
+            self.place_on_the_board = place_on_the_board
+        else:
+            print('Invalid position')
 
     def __str__(self):
-        print(f'Color : {self.color}, Place on board : {self.place_on_the_board}')
+        return (f'Color : {self.color}, Place on board : {self.place_on_the_board}')
 
     def change_color(self):
         if self.color_white == self.color:
@@ -15,18 +19,17 @@ class Chess:
         else:
             self.color = self.color_white
 
-    def check_place_on_board(self):
-        return 'Valid place' if 0 <= self.place_on_the_board[0] <= 7 and 0 <= self.place_on_the_board[1] <= 7 else 'Invalid place'
-
     def change_place_on_board(self, new_place):
         if 0 <= new_place[0] <= 7 and 0 <= new_place[1] <= 7:
             self.place_on_the_board = new_place
         else:
             print('Invalid place')
 
+
 class Rook(Chess):
     def change_place_on_board(self, new_place):
         return self.place_on_the_board[0] == new_place[0] or self.place_on_the_board[1] == new_place[1]
+
 
 class Knight(Chess):
     def change_place_on_board(self, new_place):
@@ -35,12 +38,14 @@ class Knight(Chess):
 
         return (x == 2 and y == 1) or (x == 1 or y == 2)
 
+
 class Bishop(Chess):
     def change_place_on_board(self, new_place):
         x = self.place_on_the_board[0] - new_place[0]
         y = self.place_on_the_board[1] - new_place[1]
 
         return x == y
+
 
 class Queen(Chess):
     def change_place_on_board(self, new_place):
@@ -49,12 +54,14 @@ class Queen(Chess):
 
         return x == y or self.place_on_the_board[0] == new_place[0] or self.place_on_the_board[1] == new_place[1]
 
+
 class King(Chess):
     def change_place_on_board(self, new_place):
         x = self.place_on_the_board[0] - new_place[0]
         y = self.place_on_the_board[1] - new_place[1]
 
         return x <= 1 and y <= 1
+
 
 class Pawn(Chess):
     def change_place_on_board(self, new_place):
@@ -63,6 +70,7 @@ class Pawn(Chess):
 
         return True if y == 0 and x == 1 else False
 
+
 def get_possible_moves(pieces, new_place):
     valid_pieces = []
     for piece in pieces:
@@ -70,16 +78,20 @@ def get_possible_moves(pieces, new_place):
             valid_pieces.append(piece)
     return valid_pieces
 
-Rook = Rook('white', (2,1))
-Knight = Knight('white', (5,2))
-Bishop = Bishop('white', (3,3))
-Queen = Queen('black', (3,3))
-King = King('black', (5,3))
-Pawn = Pawn('black', (6,1))
+# У наступному блоці для позначення кольорів не використовував константи, а просто строки. І також перебивав
+# назвою змінних клас, тому не було можливості створити ще один об*єкт класу.
+rook_1 = Rook(Chess.color_white, (2, 1))
+knight_1 = Knight(Chess.color_white, (5, 2))
+bishop_1 = Bishop(Chess.color_white, (3, 3))
+queen_1 = Queen(Chess.color_black, (3, 3))
+king_1 = King(Chess.color_black, (5, 3))
+pawn_1 = Pawn(Chess.color_black, (6, 1))
 
-pieces = [Rook, Knight, Bishop, Queen, King, Pawn]
-new_place = (4,4)
+pieces = [rook_1, knight_1, bishop_1, queen_1, king_1, pawn_1]
+new_place = (4, 4)
 
-result = get_possible_moves(pieces,new_place)
+result = get_possible_moves(pieces, new_place)
 for piece in result:
     print(f'{piece.color} {piece.__class__.__name__} can move to {new_place}')
+print(rook_1)
+print(king_1)
